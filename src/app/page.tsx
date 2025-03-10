e1c0f95de3a3cc395af711b74cd687c244cc5659
Shubham Gagare <shubham.gagare@siyaratechin.com>
2025-03-10 18:16:52 +0530

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div className="">
       <div className="relative flex flex-col items-start justify-center overflow-clip w-full">
         <BoxesCore className="opacity-50 w-screen h-full" />
-        <div className="flex flex-col  items-center w-full  md:px-40 py-20 px-4  relative z-10  h-fit top-[20%] pointer-events-none justify-center">
-          <span className="text-8xl mb-4 inline-block">👋</span>
+        <div className="flex flex-col  items-start w-full  md:px-40 py-20 px-4  relative z-10  h-fit top-[20%] pointer-events-none justify-center">
+          <span className="text-8xl mb-4 inline-block items-start">👋</span>
           <div className="w-full flex-col items-start">
           <h1 className="font-bold text-3xl text-foreground item-start">Welcome to</h1>
           <h1 className="text-transparent text-3xl md:text-6xl w-full md:w-fit uppercase font-bold  leading-loose tracking-wider bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] bg-clip-text ">
