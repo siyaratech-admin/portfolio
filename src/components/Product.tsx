df88ed51773f7d61958f917edbe43a8ae2fb88d0
Shubham Gagare <shubham.gagare@siyaratechin.com>
2025-03-12 14:03:44 +0530

    Updated

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -59,7 +59,7 @@ export const SingleProduct = ({ product }: { product: Product }) => {
           {product.stack?.map((stack: string) => (
             <span
               key={stack}
-              className="text-xs  md:text-xs lg:text-xs bg-gray-50 px-2 py-1 rounded-sm text-secondary"
+              className="text-xs  md:text-xs lg:text-xs bg-gray-50 px-2 py-1 rounded-sm text-black"
             >
               {stack}
             </span>
